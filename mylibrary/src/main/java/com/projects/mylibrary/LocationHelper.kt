12ee09159f5e0610fbecda93471dcf8d2b2e2acf
KeldiyarovAkhrorbek <86974406+KeldiyarovAkhrorbek@users.noreply.github.com>
2021-11-06 01:42:03 +0500

    initial commit

---
+++ mylibrary/src/main/java/com/projects/mylibrary/LocationHelper.kt
@@ -0,0 +1,16 @@
+package com.projects.mylibrary
+
+import android.location.Location
+
+class LocationHelper {
+    fun distance(
+        startLat: Double,
+        startLang: Double,
+        endLat: Double,
+        endLang: Double
+    ): Float {
+        val result = FloatArray(0)
+        Location.distanceBetween(startLat, startLang, endLat, endLang, result)
+        return result[0] / 1000
+    }
+}
