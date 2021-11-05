9a4a71035b3cc78a30475977d9ad10417be551c4
KeldiyarovAkhrorbek <86974406+KeldiyarovAkhrorbek@users.noreply.github.com>
2021-11-06 01:58:15 +0500

    changes

--- mylibrary/src/main/java/com/projects/mylibrary/LocationHelper.kt
+++ mylibrary/src/main/java/com/projects/mylibrary/LocationHelper.kt
@@ -3,14 +3,17 @@ package com.projects.mylibrary
 import android.location.Location
 
 class LocationHelper {
-    fun distance(
-        startLat: Double,
-        startLang: Double,
-        endLat: Double,
-        endLang: Double
-    ): Float {
-        val result = FloatArray(0)
-        Location.distanceBetween(startLat, startLang, endLat, endLang, result)
-        return result[0] / 1000
+    companion object {
+
+        fun distance(
+            startLat: Double,
+            startLang: Double,
+            endLat: Double,
+            endLang: Double
+        ): Float {
+            val result = FloatArray(0)
+            Location.distanceBetween(startLat, startLang, endLat, endLang, result)
+            return result[0] / 1000
+        }
     }
 }
